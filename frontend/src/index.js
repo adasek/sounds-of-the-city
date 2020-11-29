59aace9ff3845b58a81212d1e6501b10437bf5fd
Adam Benda <bendaad1@fit.cvut.cz>
2020-11-29 18:01:22 +0100

    frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,168 @@
+import * as THREE from 'three';
+
+import {OrbitControls} from './jsm/controls/OrbitControls.js';
+import {Avatar} from './jsm/avatar/Avatar.js';
+//import area from "./area.json";
+//import { Terrain } from './jsm/terrain/terrain.js';
+const area = require('../area.json')
+
+let camera, controls, scene, renderer, avatar, time;
+
+init();
+animate();
+
+function parseGeometry(geometry) {
+    //input: POINT(14.322079278 50.0925608060001)
+    let pos = geometry.match(/POINT\(([0-9.]*) ([0-9.]*)/i);
+    return {lon: parseFloat(pos[1]), lat: parseFloat(pos[2])}
+}
+
+
+function init() {
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xcccccc);
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(400, 200, 0);
+
+
+    // controls
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    avatar = new Avatar(scene, renderer.domElement)
+
+    //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+    controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.05;
+
+    controls.screenSpacePanning = false;
+
+    controls.minDistance = 100;
+    controls.maxDistance = 500;
+
+    controls.maxPolarAngle = Math.PI / 2;
+
+    // world
+    const geometry = new THREE.CylinderBufferGeometry(0, 10, 30, 4, 1);
+    const material = new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: true});
+
+    let centerLat = 50.092656
+    let centerLon = 14.3225442
+        //console.dir(area)
+        const pointTables = [
+        {
+            table: "pid_stops",
+            geometry: new THREE.CylinderBufferGeometry(0, 10, 30, 4, 1),
+            material: new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: true})
+        },
+        {
+            table: "culture_venues",
+            geometry: new THREE.CylinderBufferGeometry(5, 10, 30, 5, 1),
+            material: new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: true})
+        },
+            {
+                table: "trash_wc",
+                geometry: new THREE.CylinderBufferGeometry(5, 5, 30, 5, 1),
+                material: new THREE.MeshPhongMaterial({color: 0x666666, flatShading: true})
+            },
+            {
+                table: "trash_containers",
+                geometry: new THREE.CylinderBufferGeometry(10, 10, 30, 5, 1),
+                material: new THREE.MeshPhongMaterial({color: 0x666666, flatShading: true})
+            },
+            {
+                table: "trash_centers",
+                geometry: new THREE.CylinderBufferGeometry(5, 10, 30, 5, 1),
+                material: new THREE.MeshPhongMaterial({color: 0x666666, flatShading: true})
+            },
+            {
+                table: "police",
+                geometry: new THREE.CylinderBufferGeometry(10, 5, 30, 10, 1),
+                material: new THREE.MeshPhongMaterial({color: 0x0000ff, flatShading: true})
+            },
+            {
+                table: "nature_memorial_trees",
+                geometry: new THREE.CylinderBufferGeometry(10, 5, 30, 12, 1),
+                material: new THREE.MeshPhongMaterial({color: 0x00ff00, flatShading: true})
+            }
+        ]
+    pointTables.forEach((pointTable) => {
+            for (let i = 0; i < area[pointTable.table].length; i++) {
+                const mesh = new THREE.Mesh(pointTable.geometry, pointTable.material);
+                let point = parseGeometry(area[pointTable.table][i].geometry)
+                mesh.position.x = (point.lat - centerLat) * 100000;
+                mesh.position.y = 0;
+                mesh.position.z = (point.lon - centerLon) * 100000;
+                mesh.updateMatrix();
+                mesh.matrixAutoUpdate = false;
+                scene.add(mesh);
+            }
+        }
+    )
+
+    // Time
+    time = new Date()
+
+    // lights
+
+    const dirLight1 = new THREE.DirectionalLight(0xffffff);
+    dirLight1.position.set(1, 1, 1);
+    scene.add(dirLight1);
+
+    const dirLight2 = new THREE.DirectionalLight(0x002288);
+    dirLight2.position.set(-1, -1, -1);
+    scene.add(dirLight2);
+
+    const ambientLight = new THREE.AmbientLight(0x222222);
+    scene.add(ambientLight);
+
+    //
+
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    let currentTime = new Date();
+    let timeElapsed = currentTime - time;
+    time = currentTime
+
+    controls.update(timeElapsed);
+    let positionDiff = avatar.update(timeElapsed);
+
+    controls.target.x = avatar.object.position.x
+    controls.target.y = avatar.object.position.y
+    controls.target.z = avatar.object.position.z
+
+    controls.object.position.x -= positionDiff.x
+    controls.object.position.y -= positionDiff.y
+    controls.object.position.z -= positionDiff.z
+
+    render();
+
+}
+
+function render() {
+
+    renderer.render(scene, camera);
+
+}
---
+++ frontend/src/jsm/avatar/Avatar.js
@@ -0,0 +1,106 @@
+import {
+    EventDispatcher,
+    MOUSE,
+    Quaternion,
+    Spherical,
+    TOUCH,
+    Vector2,
+    Vector3
+} from "three";
+import * as THREE from "three";
+
+var Avatar = function ( scene, domElement ) {
+    this.domElement = domElement;
+
+    this.up = new Vector3(0,1,0)
+    this.directionAngle = 0
+    this.speedPerMs = 0.05 // per ms
+    this.rotateSpeed = 0.005 // rad per ms
+
+    const geometry = new THREE.CylinderBufferGeometry( 10, 10, 30, 3, 3 );
+    const material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } )
+
+    this.object = new THREE.Mesh( geometry, material );
+    //console.log(geometry.faces)
+    this.object.position.x = 0;
+    this.object.position.y = 0;
+    this.object.position.z = 0;
+    this.object.updateMatrix();
+
+    scene.add( this.object );
+
+    // Controls
+    this.keyTable = {left: false, right: false, forward: false, backward: false}
+
+    this.update = function () {
+
+        return function update(timeElapsed) {
+            // movement
+            let originalPosition = this.object.position.clone()
+
+            if(scope.keyTable.forward){
+                //direction
+                this.object.position.x +=  Math.sin(this.directionAngle) * this.speedPerMs * timeElapsed
+                this.object.position.z +=  Math.cos(this.directionAngle) * this.speedPerMs * timeElapsed
+            }
+            if(scope.keyTable.backward){
+                //direction
+                this.object.position.x +=  Math.sin(Math.PI + this.directionAngle) * this.speedPerMs * timeElapsed
+                this.object.position.z +=  Math.cos(Math.PI + this.directionAngle) * this.speedPerMs * timeElapsed
+            }
+            if(scope.keyTable.right){
+                this.directionAngle -=  this.rotateSpeed  * timeElapsed
+                this.object.rotation.y = this.directionAngle;
+            }
+            if(scope.keyTable.left){
+                this.directionAngle +=  this.rotateSpeed  * timeElapsed
+                this.object.rotation.y = this.directionAngle;
+            }
+
+            this.object.updateMatrix();
+            return originalPosition.sub(this.object.position);
+            //return new Vector3(0,0,0)
+        };
+
+    }();
+
+
+    var scope = this;
+
+
+    scope.domElement.ownerDocument.addEventListener("keydown", onDocumentKeyDown, false);
+    function onDocumentKeyDown(event) {
+        var keyCode = event.which;
+        if (keyCode == 87) {
+            scope.keyTable.forward = true
+        } else if (keyCode == 83) {
+            scope.keyTable.backward = true
+        } else if (keyCode == 65) {
+            scope.keyTable.left = true
+        } else if (keyCode == 68) {
+            scope.keyTable.right = true
+        }
+    }
+    scope.domElement.ownerDocument.addEventListener("keyup", onDocumentKeyUp, false);
+    function onDocumentKeyUp(event) {
+        var keyCode = event.which;
+        if (keyCode == 87) {
+            scope.keyTable.forward = false
+        } else if (keyCode == 83) {
+            scope.keyTable.backward = false
+        } else if (keyCode == 65) {
+            scope.keyTable.left = false
+        } else if (keyCode == 68) {
+            scope.keyTable.right = false
+        }
+    }
+
+    this.update();
+
+};
+
+Avatar.prototype = Object.create( EventDispatcher.prototype );
+Avatar.prototype.constructor = Avatar;
+
+
+export { Avatar };

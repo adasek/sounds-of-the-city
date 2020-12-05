4537051108a634851e398d31c3e18d8227622916
Adam Benda <bendaad1@fit.cvut.cz>
2020-12-05 18:22:57 +0100

    refactor: GeoObject

---
+++ frontend/src/jsm/world/GeoObject.js
@@ -0,0 +1,90 @@
+import * as THREE from "three";
+import * as merge from 'deepmerge';
+import {CylinderBufferGeometry, MeshPhongMaterial} from 'three';
+
+
+var GeoObject = function (geoItem, opts) {
+    this.geoItem = geoItem
+
+    const geometryClasses = {
+        CylinderBufferGeometry
+    };
+    const materialClasses = {
+        MeshPhongMaterial
+    };
+    const overwriteMerge = (destinationArray, sourceArray, options) => sourceArray
+
+    // Default values
+    this.opts = {
+        audio: {
+            'pannerModel': 'HRTF',
+            'innerCone ': 360,
+            'outerCone': 360,
+            'outerGain': 0.3,
+            'distanceModel': 'linear',
+            'maxDistance': 10000,
+            'refDistance': 1,
+            'rollOff': 10,
+        },
+        gfx: {
+            geometry: {class: 'CylinderBufferGeometry', params: [5, 5, 30, 5, 1]},
+            material: {class: 'MeshPhongMaterial', params: [{color: 0x666666, flatShading: true}]}
+        }
+    }
+    this.opts = merge(this.opts, opts, { arrayMerge: overwriteMerge })
+
+    this.parseGeometry = function (geometry) {
+        //input: POINT(14.322079278 50.0925608060001)
+        let pos = geometry.match(/POINT\(([0-9.]*) ([0-9.]*)/i);
+        return {lon: parseFloat(pos[1]), lat: parseFloat(pos[2])}
+    }
+    this.geometryCenter = function () {
+        return this.parseGeometry(this.geoItem.geometry)
+    }
+
+    this.attachToScene = function (scene, centerPoint) {
+        if (!this.opts.gfx.geometry || !this.opts.gfx.material) {
+            throw "Unknown geometry/material"
+        }
+        try {
+            let geometry = new geometryClasses[this.opts.gfx.geometry.class](...this.opts.gfx.geometry.params)
+            let material = new materialClasses[this.opts.gfx.material.class](...this.opts.gfx.material.params)
+
+            this.mesh = new THREE.Mesh(geometry, material);
+            let point = this.geometryCenter()
+            this.mesh.position.x = (point.lat - centerPoint.lat) * 100000;
+            this.mesh.position.y = 0;
+            this.mesh.position.z = (point.lon - centerPoint.lon) * 100000;
+            this.mesh.updateMatrix();
+            this.mesh.matrixAutoUpdate = false;
+            scene.add(this.mesh);
+        } catch (e) {
+            console.log(this.opts.gfx.geometry)
+            console.log(this.opts.gfx.material)
+            throw e
+        }
+    }
+
+    this.attachToAudioModel = function (audioContext, centerPoint) {
+        let point = this.geometryCenter()
+        this.panner = new PannerNode(audioContext, merge(this.opts.audio, {
+                positionX: (point.lat - centerPoint.lat) * 100000,
+                positionY: 0,
+                positionZ: (point.lon - centerPoint.lon) * 100000,
+                orientationX: 0,
+                orientationY: 1,
+                orientationZ: 0
+            })
+        )
+        if (this.opts.audio.src) {
+            this.audioElement = document.createElement('audio')
+            this.audioElement.src = this.opts.audio.src
+            this.audioElement.play()
+            this.audioTrack = audioContext.createMediaElementSource(this.audioElement);
+            this.audioTrack.connect(this.panner).connect(audioContext.destination);
+        }
+    }
+}
+
+export {GeoObject};
+
--- frontend/src/jsm/world/GeoObjectFactory.js
+++ frontend/src/jsm/world/GeoObjectFactory.js
@@ -1,123 +1,66 @@
 import * as THREE from "three";
 import {EventDispatcher} from "three";
 import {World} from "./World";
+import {GeoObject} from "./GeoObject";
 
 var GeoObjectFactory = function (controls, scene, domElement) {
 
-    // Audio
-    const pannerModel = 'HRTF';
-    const innerCone = 360;
-    const outerCone = 360;
-    const outerGain = 0.3;
-    const distanceModel = 'linear';
-    const maxDistance = 10000;
-    const refDistance = 1;
-    const rollOff = 10;
-
     this.pointTables = {
         "pid_zastavky": {
-            geometry: new THREE.CylinderBufferGeometry(0, 10, 30, 4, 1),
-            material: new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: true}),
-            soundSrc: 'sounds/213564__woodylein__at-a-bus-stop.mp3'
+            gfx: {
+                geometry: {class: 'CylinderBufferGeometry', params: [0, 10, 30, 4, 1]},
+                material: {class: 'MeshPhongMaterial', params: [{color: 0xff0000, flatShading: true}]}
+            },
+            audio: {src: 'sounds/213564__woodylein__at-a-bus-stop.mp3'}
         },
         "kultura_body": {
-            geometry: new THREE.CylinderBufferGeometry(5, 10, 30, 5, 1),
-            material: new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: true})
+            gfx: {
+                geometry: {class: 'CylinderBufferGeometry', params: [5, 10, 30, 5, 1]},
+                material: {class: 'MeshPhongMaterial', params: [{color: 0xffffff, flatShading: true}]}
+            }
         },
         "odpad_wc": {
-            geometry: new THREE.CylinderBufferGeometry(5, 5, 30, 5, 1),
-            material: new THREE.MeshPhongMaterial({color: 0x666666, flatShading: true})
+            gfx: {
+                geometry: {class: 'CylinderBufferGeometry', params: [5, 5, 30, 5, 1]},
+                material: {class: 'MeshPhongMaterial', params: [{color: 0x666666, flatShading: true}]}
+            }
         },
         "odpad_tridene_kontejnery": {
-            geometry: new THREE.CylinderBufferGeometry(10, 10, 30, 5, 1),
-            material: new THREE.MeshPhongMaterial({color: 0x666666, flatShading: true})
+            gfx: {
+                geometry: {class: 'CylinderBufferGeometry', params: [10, 10, 30, 5, 1]},
+                material: {class: 'MeshPhongMaterial', params: [{color: 0x666666, flatShading: true}]}
+            }
         },
         "odpad_sber": {
-            geometry: new THREE.CylinderBufferGeometry(5, 10, 30, 5, 1),
-            material: new THREE.MeshPhongMaterial({color: 0x666666, flatShading: true})
+            gfx: {
+                geometry: {class: 'CylinderBufferGeometry', params: [5, 10, 30, 5, 1]},
+                material: {class: 'MeshPhongMaterial', params: [{color: 0x666666, flatShading: true}]}
+            }
         },
         "policie": {
-            geometry: new THREE.CylinderBufferGeometry(10, 5, 30, 10, 1),
-            material: new THREE.MeshPhongMaterial({color: 0x0000ff, flatShading: true})
+            gfx: {
+                geometry: {class: 'CylinderBufferGeometry', params: [10, 5, 30, 10, 1]},
+                material: {class: 'MeshPhongMaterial', params: [{color: 0x0000ff, flatShading: true}]}
+            }
         },
         "priroda_pamatne_stromy": {
-            geometry: new THREE.CylinderBufferGeometry(10, 5, 30, 12, 1),
-            material: new THREE.MeshPhongMaterial({color: 0x00ff00, flatShading: true})
+            gfx: {
+                geometry: {class: 'CylinderBufferGeometry', params: [10, 5, 30, 12, 1]},
+                material: {class: 'MeshPhongMaterial', params: [{color: 0x00ff00, flatShading: true}]}
+            }
         }
     }
 
-    this.geometryType = function(geoItem){
-        if (!this.pointTables[geoItem.table_name]) {
+
+    this.build = function (geoItem, centerPoint, scene, audioCtx) {
+        if (geoItem.tableName == 'technical_usage') {
             return null
         }
-        return this.pointTables[geoItem.table_name].geometry
-    }
-    this.materialType = function(geoItem){
-        if (!this.pointTables[geoItem.table_name]) {
-            return null
-        }
-        return this.pointTables[geoItem.table_name].material
-    }
-    this.soundSrc = function(geoItem){
-        if (!this.pointTables[geoItem.table_name]) {
-            return null
-        }
-        return this.pointTables[geoItem.table_name].soundSrc
-    }
 
-    this.parseGeometry = function (geometry) {
-        //input: POINT(14.322079278 50.0925608060001)
-        let pos = geometry.match(/POINT\(([0-9.]*) ([0-9.]*)/i);
-        return {lon: parseFloat(pos[1]), lat: parseFloat(pos[2])}
+        let geoObject = new GeoObject(geoItem, this.pointTables[geoItem.table_name])
+        geoObject.attachToScene(scene, centerPoint)
+        geoObject.attachToAudioModel(audioCtx, centerPoint)
     }
-
-    this.build = function(geoItem, centerPoint, scene, audioCtx){
-        if(geoItem.tableName == 'technical_usage'){
-            return null
-        }
-        const geometryType = this.geometryType(geoItem)
-        const materialType = this.materialType(geoItem)
-        if(!geometryType || !materialType){
-            console.error("Unknown geoitem type")
-            console.error(JSON.stringify(geoItem))
-            return null
-        }
-        const mesh = new THREE.Mesh(geometryType, materialType);
-        let point = this.parseGeometry(geoItem.geometry)
-        mesh.position.x = (point.lat - centerPoint.lat) * 100000;
-        mesh.position.y = 0;
-        mesh.position.z = (point.lon - centerPoint.lon) * 100000;
-        mesh.updateMatrix();
-        mesh.matrixAutoUpdate = false;
-        scene.add(mesh);
-
-
-        const panner = new PannerNode(audioCtx, {
-            panningModel: pannerModel,
-            distanceModel: distanceModel,
-            positionX: mesh.position.x,
-            positionY: mesh.position.y,
-            positionZ: mesh.position.z,
-            orientationX: 0,
-            orientationY: 1,
-            orientationZ: 0,
-            refDistance: refDistance,
-            maxDistance: maxDistance,
-            rolloffFactor: rollOff,
-            coneInnerAngle: innerCone,
-            coneOuterAngle: outerCone,
-            coneOuterGain: outerGain
-        })
-        const soundSrc = this.soundSrc(geoItem)
-        if (soundSrc) {
-            const audioElement = document.createElement('audio')
-            audioElement.src = soundSrc
-            audioElement.play()
-            const track = audioCtx.createMediaElementSource(audioElement);
-            track.connect(panner).connect(audioCtx.destination);
-        }
-    }
-
 }
 
 export {GeoObjectFactory};
